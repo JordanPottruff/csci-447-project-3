2a28f0fbd1ba0e32d35f027cd6d383e3a8f7eb50
Lizzy Hamaoka <lizzy.hamaoka@gmail.com>
2019-11-05 14:11:53 -0700

    tanh activation function

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 class ActivationFunctions:
 
@@ -9,6 +9,9 @@ class ActivationFunctions:
     def sigmoid_prime(self, z):
         return self.sigmoid(z) * (1 - self.sigmoid(z))
 
+    def tanh(self, z):
+        return ((2 / 1 + np.exp(-2*z)) - 1)
 
-
+    def tanh_prime(self, z):
+        return 1 - np.power(self.tanh(z), 2)
 

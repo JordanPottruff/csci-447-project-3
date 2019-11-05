f8f83db9360a13e969f5cc579121d6f54aad64a8
Lizzy Hamaoka <lizzy.hamaoka@gmail.com>
2019-11-05 13:45:45 -0700

    Added sigmoid activation function

---
+++ src/activation_functions.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class ActivationFunctions:
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+
+
+

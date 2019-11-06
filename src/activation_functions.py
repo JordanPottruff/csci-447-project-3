6e79121de3d8d2dc969a90f743c428b961490e2f
Lizzy Hamaoka <lizzy.hamaoka@gmail.com>
2019-11-05 21:24:16 -0700

    updated activation functions

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+
 
 class ActivationFunctions:
 
@@ -15,3 +15,12 @@ class ActivationFunctions:
     def tanh_prime(self, z):
         return 1 - np.power(self.tanh(z), 2)
 
+    def relu(self, z):
+        return np.maximum(0, z)
+
+    # one alternative to reLU is the softplus which is an approximation of reLU
+    # def softplus(self, z):
+    #     return np.log(1.0 + np.exp(z))
+
+    # def softplus_prime(self, z):
+    #     return 1 / (1 + np.exp(-z))

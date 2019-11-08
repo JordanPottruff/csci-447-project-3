eac3cbb442fb8d57859963f10cdfb81b44142ef1
Alan Tong <alantong1234@hotmail.com>
2019-11-07 21:58:46 -0700

    added video.py

--- src/driver.py
+++ src/driver.py
@@ -92,6 +92,7 @@ def run_mfnn_classification(data_set, classes, learning_rate, momentum, converge
         for fold_i, fold in enumerate(folds):
             test = fold['test']
             train, validation = fold['train'].partition(.8)
+            print(layers)
             mfnn = MFNN(train, validation, layers, learning_rate, momentum, convergence_size, classes)
             mfnn.train()
             fold_average.append(mfnn.get_accuracy(test))
@@ -146,7 +147,7 @@ def run_mfnn_classification_data_sets():
     segmentation_data = d.get_segmentation_data("../data/segmentation.data")
     segmentation_classes = ["BRICKFACE", "SKY", "FOLIAGE", "CEMENT", "WINDOW", "PATH", "GRASS"]
 
-    run_mfnn_classification(abalone_data, abalone_classes, 1, .1, 100)
+    # run_mfnn_classification(abalone_data, abalone_classes, 1, .1, 100)
     run_mfnn_classification(car_data, car_classes, 1, .1, 100)
     run_mfnn_classification(segmentation_data, segmentation_classes, 1, .1, 100)
 
@@ -166,10 +167,10 @@ def main():
     momentum = 0.2
     convergence_size = 100
 
-    # run_mfnn_regression_data_sets(learning_rate, momentum, convergence_size)
-    # run_mfnn_classification_data_sets(learning_rate, momentum, convergence_size)
+    # run_mfnn_regression_data_sets()
+    # run_mfnn_classification_data_sets()
 
-    run_rbfnn_regression_data_sets("kmeans")
+    # run_rbfnn_regression_data_sets("kmeans")
     # run_mfnn_regression_data_sets()
 
 main()
